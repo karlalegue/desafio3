5b7668ea17a36fe3376b4f322369e099a2fc327c
karlalegue <112408672+karlalegue@users.noreply.github.com>
2022-11-29 23:47:31 -0300

    Update avance1.py

--- avance1.py
+++ avance1.py
@@ -1,8 +1,9 @@
 archivo =open(input ("Ingrese el nombre de su archivo: "))
+nombre = archivo.readline()
 lineas =archivo.read()
-lineas= lineas.replace ("\n", "")
+lineaslimpia = ""
 lineas= lineas.replace ("T", "U")
-print (lineas)
+
 
 tabla_codones={           
     "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A", "CGU": "R",
@@ -19,22 +20,46 @@ tabla_codones={
     "UGG": "W", "UAU": "Y", "UAC": "Y", "GUU": "V", "GUC": "V",
     "GUA": "V", "GUG": "V", "UAG": "STOP", "UGA": "STOP", "UAA": "STOP"}
     
+menu = input ("1. Para leer archivo Fasta y generar secuencia de proteina, ingrese 1\n2. Para obtener estadísticas de la secuencia, ingrese 2. \n3. Para generar diagrama de relación de aminoácido, ingrese 3")
+
 codon_inicio = ["A", "U", "G"]
 codon_final = "STOP"
 
+##Paso 1$$
+print (lineas)
+i = 0
+while i < len(lineas):
+    if lineas[i]=='I':
+        while lineas[i]!='F':
+            i = i + 1
+    else:
+        lineaslimpia = lineaslimpia + lineas[i]
+    i= i + 1
+print (lineaslimpia)
+
+## Paso 2
+
+codones = ""
+i = 0
+while i< len(lineaslimpia)-2:
+    if codon_inicio [0]==lineaslimpia[i] and codon_inicio [1] == lineaslimpia[i+1] and codon_inicio [2]== lineaslimpia[i+2]: 
+       while i< len(lineaslimpia)-2:
+            codones = codones + lineaslimpia[i:i+3]
+            i = i + 3
+    i = i + 1
+print(codones)
+
+##Paso 3
 sequence_proteina = ""
-
-for i in range (0, len(lineas),3):
-    if codon_inicio [0]==archivo[i] and codon_inicio [1] == archivo[i+1] and codon_inicio [2]== archivo[i+2]: 
-        codones = lineas[i:i+3]
-        sequence_proteina= sequence_proteina+tabla_codones[codones]
-    print (sequence_proteina)
-
+for i in range (0, len(codones),3):
+    codon = codones[i]+codones[i+1]+codones[i+2]
+    sequence_proteina= sequence_proteina+tabla_codones[codon]
+print (sequence_proteina)
 
 
 archivo.close()
-
+'''
 arch= open ("SEQUENCE_1_PROTEINA", "w")
 arch.write (sequence_proteina)
 arch.close()
-
+'''
